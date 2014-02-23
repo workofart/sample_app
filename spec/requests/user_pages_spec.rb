10f9b5da50b4d090e2232cdfdeb9f750cf4c2e61
Henry <hanxiangp@gmail.com>
2014-02-23 15:57:12 -0500

    finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -36,6 +36,15 @@ describe "signup" do
         fill_in "Email",        with: "user@example.com"
         fill_in "Password",     with: "foobar"
         fill_in "Confirmation", with: "foobar"
+
+    describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'user@example.com') }
+
+        it { should have_link('Sign out') }
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+      end
       end
 
       it "should create a user" do
